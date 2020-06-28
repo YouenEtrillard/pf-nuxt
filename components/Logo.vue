132b104c986c44e7d0f92738fbb1e6d94e7b5c43
csetrillard_y_home <youen.etrillard@cognix-systems.com>
2020-06-28 22:36:29 +0200

    gotta commit the first state of the project even if it looks ugly. index.vue  needs to be made ss module compatible

---
+++ components/Logo.vue
@@ -0,0 +1,119 @@
+<template>
+  <svg class="logo animate synched" viewBox="0 0 439.43 531.84">
+    <title>logo</title>
+      <path class="shape-top" d="M 40.72 72.95 l 179.03 126.6 179.88-127.1 L 219.74.54 40.72 72.95 z"></path>
+      <path class="shape-left" d="M 1.22 108.95 l 189.5 135.59-.31 284.96 L 1.22 108.95"></path>
+      <path class="shape-right" d="M 248.78 244.58 l 189.44-135.63-189.44 420.57 V 244.58 z"></path>
+  </svg>
+</template>
+<style scoped>
+  .logo {
+    fill: var(--leBlue);
+    max-width: 50vw;
+    max-height: 50vh;
+  }
+
+  @keyframes top {
+    0% {
+      fill: var(--bg);
+      stroke: var(--leBlue);
+      stroke-dasharray: 0 832;
+      stroke-dashoffset: 198;
+    }
+
+    75% {
+      fill: var(--bg);
+      stroke: var(--leBlue);
+      stroke-dasharray: 832 0;
+      stroke-dashoffset: 612;
+    }
+
+    80% {
+      fill: var(--bg);
+      stroke: var(--leBlue);
+    }
+
+    100% {
+      fill: var(--leBlue);
+      stroke: var(--leBlue);
+    }
+  }
+
+  @keyframes left {
+    0% {
+      fill: var(--bg);
+      stroke: var(--leBlue);
+      stroke-dasharray: 0 979;
+    }
+
+    75% {
+      fill: var(--bg);
+      stroke: var(--leBlue);
+      stroke-dasharray: 979 0;
+    }
+
+    80% {
+      fill: var(--bg);
+      stroke: var(--leBlue);
+    }
+
+    100% {
+      fill: var(--leBlue);
+      stroke: var(--leBlue);
+    }
+  }
+
+  @keyframes right {
+    0% {
+      fill: var(--bg);
+      stroke: var(--leBlue);
+      stroke-dashoffset: -979px;
+    }
+
+    75% {
+      fill: var(--bg);
+      stroke: var(--leBlue);
+      stroke-dashoffset: 0;
+    }
+
+    80% {
+      fill: var(--bg);
+      stroke: var(--leBlue);
+    }
+
+    100% {
+      fill: var(--leBlue);
+      stroke: var(--leBlue);
+    }
+  }
+
+  .animate {
+    --duration: 2.5s;
+    --delay: 0.35s;
+  }
+
+  .synched {
+    --delay: 0s;
+  }
+
+  .animate .shape-top {
+    animation: top var(--duration) linear;
+    animation-fill-mode: forwards;
+  }
+
+  .animate .shape-left {
+    --self-delay: var(--delay);
+    --self-duration: calc(var(--duration) - var(--self-delay));
+    animation: left var(--self-duration) linear var(--self-delay);
+    animation-fill-mode: both;
+    stroke-dashoffset: -234px;
+  }
+
+  .animate .shape-right {
+    --self-delay: calc(var(--delay) * 2);
+    --self-duration: calc(var(--duration) - var(--self-delay));
+    stroke-dasharray: 979px;
+    animation: right var(--self-duration) linear var(--self-delay);
+    animation-fill-mode: both;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,142 @@
+<template :class="[ $style.root ]">
+  <section :class="[
+      $style.u-wrapper,
+      $style.landing
+    ]"
+    appAppear
+  >
+    <section :class="$style.introduction">
+      <h1>Hi,<br>I'm Youen,<br>web developer</h1>
+      <p :class="$style.introSub">Front End Developer / Ionic / Angular</p>
+      <a routerLink="/about" :class="$style.btnSmart" title="But who am I ?">
+        <span :class="[$style.btn-iconWrapper]">
+          <app-custom-icon icon="who-am-i"></app-custom-icon>
+        </span>
+        <span :class="[$style.btn-text]">But, who am I ?</span>
+        <span :class="[
+            $style.btn-smart-border,
+            $style.btn-smart-border-top,
+          ]"
+        ></span>
+        <span :class="[
+            $style.btn-smart-border,
+            $style.btn-smart-border-right
+          ]"
+        ></span>
+        <span :class="[
+            $style.btn-smart-border,
+            $style.btn-smart-border-bottom
+          ]"
+        ></span>
+        <span :class="[
+            $style.btn-smart-border,
+            $style.btn-smart-border-left
+          ]"
+        ></span>
+      </a>
+    </section>
+    <section :class="$style.logo-showcase">
+      <div :class="$style.logo-wrapper">
+        <logo />
+        <app-custom-icon icon="logo"></app-custom-icon> <!-- composant à recréer -->
+      </div>
+    </section>
+    <div :class="$style.logo-background">
+      <app-custom-icon icon="logo"></app-custom-icon> <!-- composant à recréer -->
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Logo from '~/components/Logo.vue';
+
+  export default Vue.extend({
+    components: {
+      Logo,
+    }
+  })
+</script>
+
+<style module>
+  @import "@/assets/conf.scss";
+
+  .root {
+    width: 100%;
+    overflow: hidden;
+  }
+
+  .landing {
+    position: relative;
+    height: 100vh;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+  }
+
+  .logo-background {
+    position: absolute;
+    bottom: 30%;
+    right: 25%;
+    height: 150%;
+    width: 100%;
+    z-index: 0;
+    transform: translateX(50%) translateY(50%);
+    opacity: 0.2;
+    pointer-events: none;
+    transition: opacity .25s ease-out;
+
+    @include breakpoint($smallTablet) {
+      opacity: 0;
+    }
+  }
+
+  .logo-showcase {
+    width: 45rem;
+    position: absolute;
+    bottom: 50%;
+    right: 25%;
+    z-index: 1;
+    transform: translateX(50%) translateY(50%);
+    transition: opacity .25s ease-out;
+
+    @include breakpoint($smallTablet, up) {
+      fill: $main-color;
+    }
+
+    @include breakpoint($smallTablet) {
+      opacity: 0.2;
+    }
+
+    &::before {
+      content: '';
+      display: block;
+      width: 100%;
+      padding-top: calc(55%/45*100);
+    }
+  }
+
+  .logo-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .introduction {
+    color: var(--text-color-light);
+    flex: 50% 0 0;
+    z-index: 10;
+
+    @include breakpoint($smallTablet) {
+      flex-grow: 1;
+    }
+  }
+
+  .introSub {
+    font-size: 2.4rem;
+    font-size: var(--fz-subtitle);
+    opacity: 0.7;
+  }
+</style>
