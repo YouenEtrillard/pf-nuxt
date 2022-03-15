06df5cc4b0e896343519ac3368ea7db7276dbe11
YouenEtrillard <contact@youen-etrillard.com>
2022-03-15 18:51:06 +0100

    fix page name in browser tab

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,6 +15,33 @@ export default Vue.extend({
   components: {
     CustomIcon,
     SidebarElem
+  },
+  methods: {
+    getPageName() {
+      let routeName;
+
+      if (this.$route.name === 'works-url') {
+        const words = this.$route.params.url.split('-');
+        routeName = words
+          .map((word) => {
+            return word[0].toUpperCase() + word.substring(1);
+          })
+          .join(' ');
+      } else {
+        routeName =
+          this.$route.name === 'index'
+            ? 'Home'
+            : this.$route.name.charAt(0).toUpperCase() +
+              this.$route.name.slice(1);
+      }
+
+      return `${routeName} | Youen`;
+    }
+  },
+  head() {
+    return {
+      title: this.getPageName()
+    };
   }
 });
 </script>
